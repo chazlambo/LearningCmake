3a867c00dd0c7a28da353e1c6f6641740b303f44
Charlie Lambert <charlie.lambert@gmail.com>
2023-01-26 23:39:22 -0700

    First Commit

---
+++ WriteToFile/build/CMakeFiles/write_to_file.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/write_to_file.dir/WriteToFile.cpp.o"
+  "CMakeFiles/write_to_file.dir/WriteToFile.cpp.o.d"
+  "write_to_file"
+  "write_to_file.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/write_to_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
